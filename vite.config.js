ff16443e09ee2c7fd7f5b5118c7f24474f60b105
kartik20144 <122911965+kartik20144@users.noreply.github.com>
2024-06-19 13:22:48 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist', // Specify the publish directory here
+    assetsDir: '', // Optional: Directory for assets within outDir
+    // Other build options
+  },
+})
